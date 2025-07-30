4ed2b738a65c66fab5577b210e4b057164f85fed
Vikash pandit <vikash.pandit0444@gmail.com>
2025-07-30 20:40:34 +0530

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 Your Name. All rights reserved.</p>
+      <p>© 2025 Vikash Prajapati. All rights reserved.</p>
     </footer>
   );
 }
